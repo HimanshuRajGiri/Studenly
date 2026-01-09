41eead24f9a099a0934bd96cda1e2a362a7c3a75
shrutikumari18 <cshruti359@gmail.com>
2026-01-09 13:05:06 +0530

    hello

--- script.js
+++ script.js
@@ -2,61 +2,19 @@ const inputTask = document.getElementById("inputTask");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 
-todoAdd.addEventListener("click", () => {
-    if(todoInput.value.trim() !== "") {
-        const li = document.createElement("li");
-        li.textContent = todoInput.value;
-        li.addEventListener("click", () => li.remove()); // click to delete
-        todoList.appendChild(li);
-        todoInput.value = "";
+addTaskBtn.addEventListener("click", function () {
+    const taskText = inputTask.value;
+
+    if(taskText === ""){
+        alert("please enter any task.");
+        return;
     }
-});
 
-// Expense Tracker
-const expName = document.getElementById("exp-name");
-const expAmount = document.getElementById("exp-amount");
-const expAdd = document.getElementById("exp-add");
-const expList = document.getElementById("exp-list");
-const expTotal = document.getElementById("exp-total");
-let total = 0;
+    const list = document.createElement("list");
+    list.textContent = taskText;
 
-expAdd.addEventListener("click", () => {
-    const name = expName.value.trim();
-    const amount = parseFloat(expAmount.value);
-    if(name && !isNaN(amount)) {
-        const li = document.createElement("li");
-        li.textContent = `${name}: $${amount.toFixed(2)}`;
-        expList.appendChild(li);
-        total += amount;
-        expTotal.textContent = total.toFixed(2);
-        expName.value = "";
-        expAmount.value = "";
-    }
-});
+    taskList.appendChild(list);
 
-// Notes
-const noteInput = document.getElementById("note-input");
-const noteAdd = document.getElementById("note-add");
-const noteList = document.getElementById("note-list");
+    inputTask.value="";
 
-noteAdd.addEventListener("click", () => {
-    if(noteInput.value.trim() !== "") {
-        const li = document.createElement("li");
-        li.textContent = noteInput.value;
-        li.addEventListener("click", () => li.remove()); // click to delete
-        noteList.appendChild(li);
-        noteInput.value = "";
-    }
-});
-
-// Habits
-const habits = document.querySelectorAll(".habit");
-habits.forEach(habit => {
-    habit.addEventListener("change", () => {
-        if(habit.checked){
-            habit.parentElement.style.textDecoration = "line-through";
-        } else {
-            habit.parentElement.style.textDecoration = "none";
-        }
-    });
 });
